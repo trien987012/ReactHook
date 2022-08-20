fc9403d5338ff391db7df00f9d05c54c7511445b
trien987012 <phattrien201000@gmail.com>
2022-08-20 14:32:20 +0700

    create

---
+++ src/components/headerHome/HeaderHome.jsx
@@ -0,0 +1,37 @@
+//rfc
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const HeaderHome = () => {
+    return (
+<div>
+  <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    <NavLink className="navbar-brand" to="/">Project Hook</NavLink>
+    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
+    <div className="collapse navbar-collapse" id="collapsibleNavId">
+      <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home <span className="visually-hidden">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Hook</a>
+          <div className="dropdown-menu" aria-labelledby="dropdownId">
+            <NavLink className="dropdown-item" to="/usestate">UseState</NavLink>
+          </div>
+        </li>
+      </ul>
+      <form className="d-flex my-2 my-lg-0">
+        <input className="form-control me-sm-2" type="text" placeholder="Search" />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</div>
+
+    );
+}
+
+export default HeaderHome;
